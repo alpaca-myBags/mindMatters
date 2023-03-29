785f98c4922a43b97f672e46fba97628ad758ff8
duracellB <i.schlae.h@gmail.com>
2023-03-29 15:18:29 -0400

    working on it

--- script.js
+++ script.js
@@ -22,21 +22,19 @@ document.addEventListener("DOMContentLoaded", async () => {
   console.log("loaded content, + quote data loaded");
 });
 
-// ----- Quote api ----- 
+// ----- Quote api -----
 
 const api_url = "https://type.fit/api/quotes";
-const quoteContainer = document.getElementById('quote-container');
+const quoteContainer = document.getElementById("quote-container");
 
 fetch("https://type.fit/api/quotes")
   .then(response => response.json())
   .then(data => {
     let index = Math.floor(Math.random() * 1643);
     console.log(data[index]);
-    quoteContainer.innerText = `${data[index].text} - ${data[index].author}`
+    quoteContainer.innerText = `${data[index].text} - ${data[index].author}`;
   })
   .catch(error => console.log(error));
-
-
 
 /// QUERY SELECTORS ///
 document.querySelectorAll(".circle").forEach(circle => {
